ab48eb721b785f7cb5bc65cb5df9c4295d3688a1
gerenciadoc <contato@pontaconsultoria.com>
2025-05-27 23:03:05 -0300

    Create Inicialização .bash

---
+++ Inicialização .bash
@@ -0,0 +1,8 @@
+cd gerenciadoc
+git init
+npm init -y
+# Copiar todos os arquivos criados
+npm install
+npx shadcn-ui@latest init
+git add .
+git commit -m "feat: setup inicial do projeto GerenciaDoc"
